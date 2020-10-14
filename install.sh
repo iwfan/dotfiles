0a34688fbbce6c6a22c41aef79afbf88a1045c15
iwfan <i.wangfancn@gmail.com>
2020-10-14 13:41:42 +0800

    update zs config

--- install.sh
+++ install.sh
@@ -15,15 +15,18 @@ info () {
 }
 
 # prerequirments package
+
+#brew install iterm2
+#brew tap homebrew/cask-fonts && brew cask install font-sauce-code-pro-nerd-font
+
 packages=(
-  iterm2
   git
   zsh
   tmux
   node
-  ruby
-  python3
   golang
+#  python3
+#  ruby
   ripgrep
   fzf
   the_silver_searcher
@@ -33,14 +36,20 @@ packages=(
   highlight
   neovim
   lazygit
-  lazynpm
+#  lazynpm
 )
 
 for i in "${packages[@]}"
 do
-  # brew install $i
-  # echo "---------------------------------------------------------"
+  #brew install $i
+  echo "---------------------------------------------------------"
 done
+
+# change default shell to zsh
+if [[ $SHELL != *"zsh"* ]]; then
+    chsh -s $(which zsh)
+    success "change default shell to zsh"
+fi
 
 # tpm
 if [ ! -d ~/.tmux/plugins/tpm ]; then
@@ -48,24 +57,31 @@ if [ ! -d ~/.tmux/plugins/tpm ]; then
     success "install tpm"
 fi
 
+# zimfw
 
-ln -fs ./tmux/tmux.conf ~/.tmux.conf
+if [ ! -d ~/.zim ]; then
+    curl -fsSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh | zsh
+    success "install zimfw"
+fi
 
-ln -fs ./_ideavimrc ~/.ideavimrc
-ln -fs ./nvim ~/.config/nvim
 
-ln -fs ./hammerspoon ~/.hammerspoon
+rm ~/.tmux.conf
+ln -s ~/dotfiles/tmux/tmux.conf ~/.tmux.conf
 
-ln -fs ./zsh/p10k.zsh ~/.p10k.zsh
-ln -fs ./zsh/zimrc ~/.zimrc
-ln -fs ./zsh/zshrc ~/.zshrc
+rm ~/.ideavimrc
+ln -s ~/dotfiles/_ideavimrc ~/.ideavimrc
 
-source ~/.zshrc
+rm -rf ~/.config/nvim
+ln -s ~/dotfiles/nvim ~/.config/nvim
+
+rm -rf ~/.hammerspoon
+ln -s ~/dotfiles/hammerspoon ~/.hammerspoon
+
+rm ~/.p10k.zsh ~/.zimrc ~/.zshrc
+ln -s ~/dotfiles/zsh/p10k.zsh ~/.p10k.zsh
+ln -s ~/dotfiles/zsh/zimrc ~/.zimrc
+ln -s ~/dotfiles/zsh/zshrc ~/.zshrc
 
 success "link custom config file to $HOME"
 
-# change default shell to zsh
-if [[ $SHELL != *"zsh"* ]]; then
-    chsh -s $(which zsh)
-    success "change default shell to zsh"
-fi
+#source ~/.zshrc
