c9401fd71c511ce7052fe3c3465bbd2280d5de88
iwfan <i.wangfancn@gmail.com>
2020-12-27 23:12:45 +0800

    update plugin

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -32,8 +32,10 @@ return require('packer').startup(function()
   use 'tpope/vim-obsession'
   use 'tpope/vim-commentary'
   use 'tpope/vim-surround'
+  use 'tpope/vim-characterize'
   use 'tpope/vim-scriptease'
   use 'tpope/vim-dispatch'
+
   use 'theprimeagen/vim-be-good'
 
   use 'neovim/nvim-lspconfig'
