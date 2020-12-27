a13e6e39176f15f8fe26014306396911439efe26
iwfan <i.wangfancn@gmail.com>
2020-12-27 22:23:24 +0800

    update plugin

---
+++ nvim/init.lua
@@ -0,0 +1,21 @@
+--
+-- ███╗   ███╗██╗   ██╗██╗   ██╗██╗███╗   ███╗██████╗  ██████╗
+-- ████╗ ████║╚██╗ ██╔╝██║   ██║██║████╗ ████║██╔══██╗██╔════╝
+-- ██╔████╔██║ ╚████╔╝ ██║   ██║██║██╔████╔██║██████╔╝██║
+-- ██║╚██╔╝██║  ╚██╔╝  ╚██╗ ██╔╝██║██║╚██╔╝██║██╔══██╗██║
+-- ██║ ╚═╝ ██║   ██║    ╚████╔╝ ██║██║ ╚═╝ ██║██║  ██║╚██████╗
+-- ╚═╝     ╚═╝   ╚═╝     ╚═══╝  ╚═╝╚═╝     ╚═╝╚═╝  ╚═╝ ╚═════╝
+-- Author:   Zi莱卷 <i.wangfancn@gmail.com>
+-- Github:   https://github.com/iwfan
+-- License:  MIT License
+
+require 'settings'
+require 'keymaps'
+
+local VIM_SCRIPT_PATH = vim.fn.expand('<sfile>:p:h')
+vim.fn['helpers#source_dir'](VIM_SCRIPT_PATH .. '/general/plug-conf')
+
+require 'plugins'
+require 'plugs'
+
+-- lua require 'functions'
--- nvim/lua/plugs/bufferline.lua
+++ nvim/lua/plugs/bufferline.lua
@@ -12,3 +12,4 @@ vim.api.nvim_set_keymap('n', 'b<', ':BufferLineMovePrev<CR>', { noremap = true, 
 vim.api.nvim_set_keymap('n', 'b>', ':BufferLineMoveNext<CR>', { noremap = true, silent = true })
 
 vim.api.nvim_set_keymap('n', 'gb', ':BufferLinePick<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<BS>b', ':Bdelete<CR>', { noremap = true, silent = true })
