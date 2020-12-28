14c5b2fb4c6e15d25c811aa2d90484ac07834682
iwfan <i.wangfancn@gmail.com>
2020-12-28 22:56:58 +0800

    remove unused conf

--- nvim/init.lua
+++ nvim/init.lua
@@ -11,10 +11,6 @@
 
 require 'settings'
 require 'keymaps'
-
-local VIM_SCRIPT_PATH = vim.fn.expand('<sfile>:p:h')
-vim.fn['helpers#source_dir'](VIM_SCRIPT_PATH .. '/general/plug-conf')
-
 require 'plugins'
 require 'plugs'
 require 'functions'
