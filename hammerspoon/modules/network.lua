85dac5e7ed5d81c04ab925f5e90695e524f73656
iwfan <i.wangfancn@gmail.com>
2021-03-17 23:49:57 +0800

    update hammerspoons network script

--- hammerspoon/modules/network.lua
+++ hammerspoon/modules/network.lua
@@ -5,13 +5,12 @@ function connect_tw_vpn(config)
     local password = config.password
     local secret_key = config.secret_key
     local domain = config.domain
-    local connection_address = domain or string.format("%s.vpn.thoughtworks.com", region)
     local authentication_method_index = "2"
 
     local command_of_get_code = string.format("oathtool --totp -b %s", secret_key)
     local code = hs.execute(command_of_get_code, true)
     local user_info = string.format("printf '%s\n%s\n%s\n%s\n'", username, password, authentication_method_index, code)
-    local connection_info = string.format("/opt/cisco/anyconnect/bin/vpn -s connect %s", connection_address)
+    local connection_info = string.format("/opt/cisco/anyconnect/bin/vpn -s connect %s", domain)
 
     hs.execute("/opt/cisco/anyconnect/bin/vpn disconnect", true)
     hs.execute(user_info .. " | " .. connection_info, true)
@@ -25,7 +24,6 @@ hs.hotkey.bind(
         local config = require('../config')
         hs.execute("say 'Ready to connect'")
         connect_tw_vpn(config)
-        -- connect_tw_vpn("cn")
         hs.alert.show("ThoughtWorks VPN successful connection")
         hs.execute("say 'ThoughtWorks VPN successful connection'")
     end
