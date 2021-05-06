09bbc9a6d6894c57d407a72b1c3d64c76b8a3d5f
iwfan <i.wangfancn@gmail.com>
2021-05-06 17:51:37 +0800

    add volumnScrool

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -5,7 +5,9 @@ _G.leader = {"alt"}
 -- 定义默认加载的 Spoons
 local hspoon_list = {
     "ModalMgr",
-    "WinWin", -- 窗口管理,
+    "WinWin",
+    "KSheet",
+    "VolumeScroll"
 }
 
 -- 加载 Spoons
@@ -18,10 +20,12 @@ require('modules.config-reload')
 require('modules.window-management')
 require('modules.clipboard')
 require('modules.app-hints')
--- require('modules/network')
+require('modules.cheatsheet')
 -- require('modules/pomodoro')
 -- require('modules.markdown')
 
+spoon.VolumeScroll:init()
+spoon.VolumeScroll:start(leader)
 
 spoon.ModalMgr.supervisor:bind(hyper, "Z", "Toggle Hammerspoon Console", function() hs.toggleConsole() end)
 spoon.ModalMgr.supervisor:enter()
