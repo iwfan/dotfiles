eb7628eec72ddb1984bd83ff94af37d6def2810c
iwfan <i.wangfancn@gmail.com>
2021-05-08 13:11:04 +0800

    update neovim config

--- nvim/lua/plugs/appearance.lua
+++ nvim/lua/plugs/appearance.lua
@@ -1,13 +1,6 @@
 local M = {}
 
 function M.dashboard()
-  -- local home = os.getenv('HOME')
-  -- vim.g.dashboard_footer_icon = '🐬 '
-  -- vim.g.dashboard_preview_command = 'cat'
-  -- vim.g.dashboard_preview_pipeline = 'lolcat -F 0.3'
-  -- vim.g.dashboard_preview_file = home .. '/.config/nvim/static/neovim.cat'
-  -- vim.g.dashboard_preview_file_height = 12
-  -- vim.g.dashboard_preview_file_width = 80
   vim.g.dashboard_default_executive = "telescope"
   vim.g.dashboard_custom_section = {
     new_file = {
