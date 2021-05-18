af7033c4d65ee3df044730cdf5dccfca357cb411
iwfan <i.wangfancn@gmail.com>
2021-05-18 13:10:54 +0800

    update fzf conf

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -5,9 +5,15 @@ export EDITOR=nvim
 export GOPATH=$HOME/go
 export PATH="/usr/local/sbin:$GOPATH/bin:$PATH"
 
-export FZF_DEFAULT_COMMAND='fd'
+FD_OPTIONS="--follow --exclude .git --exclude node_modules"
+export FZF_DEFAULT_OPTS="--height 50% --reverse --multi \
+  --preview='[[ \$(file --mime {}) =~ binary ]] && echo {} is a binary file \
+  || (ccat --color=always {} || cat {}) 2> /dev/null | head -300' \
+  --bind='ctrl-d:half-page-down,ctrl-u:half-page-up'"
+export FZF_DEFAULT_COMMAND="git ls-files --cached --others --exclude-standard | fd --type f --type -l $FD_OPTIONS"
+export FZF_CTRL_T_COMMAND="fd $FD_OPTIONS"
+export FZD_ALT_C_COMMAND="fd --type d $FD_OPTIONS"
 export FZF_COMPLETION_TRIGGER='/'
-export FZF_DEFAULT_OPTS='--preview "ccat --color=always {}"'
 export FZF_TMUX=1
 export FZF_TMUX_HEIGHT='80%'
 
