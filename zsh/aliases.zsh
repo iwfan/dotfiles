0201eae76f247aa172ddf8aa5bd09896e7a63511
Zi莱卷 <i.wangfancn@gmail.com>
2021-06-01 23:43:01 +0800

    Update aliases.zsh

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -5,15 +5,17 @@ alias .....="cd ../../../.."
 alias ~="cd ~"
 alias -- -="cd -"
 
-alias vi="/usr/local/bin/nvim"
-alias nv="/usr/local/bin/nvim"
-alias v="/usr/local/bin/nvim +SessionLoad"
-alias e="/usr/local/bin/nvim"
+alias v="nvim +SessionLoad"
 alias ls="lsd"
 alias ll="ls -l"
 alias la="ls -la"
 alias lt="ls --tree"
+
 alias n="pnpm"
+alias ni="pnpm i"
+alias ns="pnpm run start"
+alias nd="pnpm run dev"
+alias nt="pnpm run test"
 
 ################[ todo: todo items (https://github.com/todotxt/todo.txt-cli) ]################
 alias tdo="todo.sh"
