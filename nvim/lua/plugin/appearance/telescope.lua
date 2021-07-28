2453e962093f65ef5923b01c39d89134b56dc8cc
iwfan <i.wangfancn@gmail.com>
2021-07-28 11:31:27 +0800

    update neovim conf

--- nvim/lua/plugin/appearance/telescope.lua
+++ nvim/lua/plugin/appearance/telescope.lua
@@ -21,7 +21,6 @@ require("telescope").setup {
     file_sorter          = require "telescope.sorters".get_fuzzy_file,
     file_ignore_patterns = {},
     generic_sorter       = require "telescope.sorters".get_generic_fuzzy_sorter,
-    path_display         = true,
     winblend             = 10,
     preview_height       = 1,
     preview_width        = 0.8,
