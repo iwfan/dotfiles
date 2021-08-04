f7c5312bfa1b285d1375d97dd7d0d913a8a063c9
iwfan <i.wangfancn@gmail.com>
2021-08-04 14:57:59 +0800

    update neovim conf

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -1,108 +1,113 @@
 -- Environment
-vim.g.python_host_prog         = '/usr/bin/python'
-vim.g.python3_host_prog        = '/usr/local/bin/python3'
-
+vim.g.python_host_prog = '/usr/bin/python'
+vim.g.python3_host_prog = '/usr/local/bin/python3'
 
 -- Disable built-in plugins
-vim.g.loaded_gzip              = 1
-vim.g.loaded_tar               = 1
-vim.g.loaded_tarPlugin         = 1
-vim.g.loaded_zip               = 1
-vim.g.loaded_zipPlugin         = 1
-vim.g.loaded_getscript         = 1
-vim.g.loaded_getscriptPlugin   = 1
-vim.g.loaded_vimball           = 1
-vim.g.loaded_vimballPlugin     = 1
-vim.g.loaded_matchit           = 1
-vim.g.loaded_matchparen        = 1
-vim.g.loaded_2html_plugin      = 1
-vim.g.loaded_logiPat           = 1
-vim.g.loaded_rrhelper          = 1
-vim.g.loaded_netrw             = 1
-vim.g.loaded_netrwPlugin       = 1
-vim.g.loaded_netrwSettings     = 1
+vim.g.loaded_gzip = 1
+vim.g.loaded_tar = 1
+vim.g.loaded_tarPlugin = 1
+vim.g.loaded_zip = 1
+vim.g.loaded_zipPlugin = 1
+vim.g.loaded_getscript = 1
+vim.g.loaded_getscriptPlugin = 1
+vim.g.loaded_vimball = 1
+vim.g.loaded_vimballPlugin = 1
+vim.g.loaded_matchit = 1
+vim.g.loaded_matchparen = 1
+vim.g.loaded_2html_plugin = 1
+vim.g.loaded_logiPat = 1
+vim.g.loaded_rrhelper = 1
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+vim.g.loaded_netrwSettings = 1
 vim.g.loaded_netrwFileHandlers = 1
-
 
 -- Options
 
 -- general opt
-vim.opt.fileformats    = "unix,mac,dos"
-vim.opt.lazyredraw     = true
-vim.opt.autowrite      = true
-vim.opt.autowriteall   = true
-vim.opt.backup         = false
-vim.opt.writebackup    = false
-vim.opt.swapfile       = false
-vim.opt.undofile       = true
--- vim.opt.mouse          = "a"
-vim.opt.magic          = true
-vim.opt.errorbells     = true
-vim.opt.visualbell     = true
-vim.opt.shortmess:append { c = true, S = true }
+vim.opt.fileformats = "unix,mac,dos"
+vim.opt.lazyredraw = true
+vim.opt.autowrite = true
+vim.opt.autowriteall = true
+vim.opt.backup = false
+vim.opt.writebackup = false
+vim.opt.swapfile = false
+vim.opt.undofile = true
+vim.opt.mouse = "ni"
+vim.opt.magic = true
+vim.opt.errorbells = true
+vim.opt.visualbell = true
+vim.opt.shortmess:append{c = true, S = true}
 
 -- behavior opt
-vim.opt.clipboard      = "unnamedplus"
-vim.opt.termguicolors  = true
+vim.opt.clipboard = "unnamedplus"
+vim.opt.termguicolors = true
 vim.opt.relativenumber = true
-vim.opt.number         = true
-vim.opt.cursorline     = true
-vim.opt.showmode       = false
-vim.opt.showcmd        = true
-vim.opt.showtabline    = 2
-vim.opt.signcolumn     = "yes"
-vim.opt.scrolloff      = 5
-vim.opt.sidescrolloff  = 5
-vim.opt.virtualedit    = "block"
-vim.opt.splitbelow     = true
-vim.opt.splitright     = true
-vim.opt.joinspaces     = false
-vim.opt.foldlevel      = 1
-vim.opt.foldnestmax    = 10
+vim.opt.number = true
+vim.opt.cursorline = true
+vim.opt.showmode = false
+vim.opt.showcmd = true
+vim.opt.showtabline = 2
+vim.opt.signcolumn = "yes"
+vim.opt.scrolloff = 5
+vim.opt.sidescrolloff = 5
+vim.opt.virtualedit = "block"
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.joinspaces = false
+vim.opt.foldlevel = 1
+vim.opt.foldnestmax = 10
 vim.opt.foldlevelstart = 99
 
 -- tabs & indent
-vim.opt.tabstop        = 2
-vim.opt.softtabstop    = -1
-vim.opt.shiftwidth     = 2
-vim.opt.expandtab      = true
-vim.opt.smartindent    = true
-vim.opt.shiftround     = true
+vim.opt.tabstop = 2
+vim.opt.softtabstop = -1
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.shiftround = true
 
 -- search
-vim.opt.ignorecase     = true
-vim.opt.smartcase      = true
-vim.opt.infercase      = true
-vim.opt.incsearch      = true
-vim.opt.hlsearch       = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.infercase = true
+vim.opt.incsearch = true
+vim.opt.hlsearch = true
 
 -- line break
-vim.opt.colorcolumn    = "+1"
-vim.opt.wrap           = true
-vim.opt.whichwrap      = "b,s,h,l,<,>,[,],~"
-vim.opt.showbreak      = "↪ "
+vim.opt.colorcolumn = "+1"
+vim.opt.wrap = true
+vim.opt.whichwrap = "b,s,h,l,<,>,[,],~"
+vim.opt.showbreak = "↪ "
 
-vim.opt.list           = true
-vim.opt.listchars      = { tab = " ", trail = "·", precedes = "←", extends = "→", nbsp = "␣" }
-vim.opt.fillchars      = { fold = "", eob = " " }
-vim.opt.pumblend       = 10
-vim.opt.winblend       = 10
-vim.opt.pumheight      = 20
-vim.opt.hidden         = true
-vim.opt.switchbuf      = {"useopen", "vsplit"}
-vim.opt.completeopt    = { 'menu', 'menuone', 'noselect' }
+vim.opt.list = true
+vim.opt.listchars = {
+    tab = " ",
+    trail = "·",
+    precedes = "←",
+    extends = "→",
+    nbsp = "␣"
+}
+vim.opt.fillchars = {fold = "", eob = " "}
+vim.opt.pumblend = 10
+vim.opt.winblend = 10
+vim.opt.pumheight = 20
+vim.opt.hidden = true
+vim.opt.switchbuf = {"useopen", "vsplit"}
+vim.opt.completeopt = {'menu', 'menuone', 'noselect'}
 vim.opt.wildignorecase = true
-vim.opt.wildignore     = {
-  ".git", ".hg", ".svn", "*.pyc", "*.o", "*.out", "*.jpg", "*.jpeg", "*.png", "*.gif",
-  "*.zip", "**/tmp/**", "*.DS_Store", "**/node_modules/**"}
+vim.opt.wildignore = {
+    ".git", ".hg", ".svn", "*.pyc", "*.o", "*.out", "*.jpg", "*.jpeg", "*.png",
+    "*.gif", "*.zip", "**/tmp/**", "*.DS_Store", "**/node_modules/**"
+}
 
 -- autocmd
 vim.cmd 'au BufEnter * set formatoptions-=cro formatoptions+=j1'
 vim.cmd 'au TextYankPost * silent! lua vim.highlight.on_yank({timeout=500})'
-vim.cmd[[autocmd BufReadPost * lua Goto_last_pos()]]
+vim.cmd [[autocmd BufReadPost * lua Goto_last_pos()]]
 function Goto_last_pos()
-  local last_pos = vim.fn.line("'\"")
-  if last_pos > 0 and last_pos <= vim.fn.line("$") then
-    vim.api.nvim_win_set_cursor(0, {last_pos, 0})
-  end
+    local last_pos = vim.fn.line("'\"")
+    if last_pos > 0 and last_pos <= vim.fn.line("$") then
+        vim.api.nvim_win_set_cursor(0, {last_pos, 0})
+    end
 end
