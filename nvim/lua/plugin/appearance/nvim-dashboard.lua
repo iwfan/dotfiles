71363f6ebc4e49db2a225efa6f22f8af59a7b5c2
iwfan <i.wangfancn@gmail.com>
2021-08-12 23:06:54 +0800

    update neovim session restore

--- nvim/lua/plugin/appearance/nvim-dashboard.lua
+++ nvim/lua/plugin/appearance/nvim-dashboard.lua
@@ -1,35 +1,36 @@
-vim.g.dashboard_default_executive = 'telescope'
+vim.g.dashboard_default_executive = "telescope"
 
 vim.g.dashboard_custom_section = {
     a = {
-        description = {'  Reload Last Session            SPC r s'},
-        command = 'SessionLoad'
+        description = { "  Reload Last Session            SPC r s" },
+        command = "SessionLoad",
     },
     b = {
-        description = {'  Recently Opened Files          SPC f o'},
-        command = 'Telescope oldfiles'
+        description = { "  Recently Opened Files          SPC f o" },
+        command = "Telescope oldfiles",
     },
     c = {
-        description = {'  Open Project                   SPC f p'},
-        command = 'Telescope project'
+        description = { "  Open Project                   SPC f p" },
+        command = "Telescope project",
     },
     d = {
-        description = {'  Find File                      SPC f f'},
-        command = 'Telescope find_files'
+        description = { "  Find File                      SPC f f" },
+        command = "Telescope find_files",
     },
     e = {
-        description = {'  Open Neovim Configuration             '},
-        command = ':e ~/.config/nvim/init.lua'
-    }
+        description = { "  Open Neovim Configuration             " },
+        command = ":e ~/.config/nvim/init.lua",
+    },
 }
 
 function _G.save_session_and_exit()
-    vim.cmd("wa!")
-    vim.cmd("SessionSave")
     vim.cmd("xa!")
 end
 
-vim.cmd('command! -nargs=0 Q call v:lua.save_session_and_exit()')
+vim.cmd("command! -nargs=0 Q call v:lua.save_session_and_exit()")
+
+autocmd("VimEnter", "*", "SessionLoad")
+autocmd("VimLeave", "*", "SessionSave")
 
 vim.g.dashboard_custom_header = {
     "       ▄▄        ▄▄ ",
@@ -41,9 +42,13 @@ vim.g.dashboard_custom_header = {
     "   ▌   ▐   ▀█████▄████     ▌     ▐▌ █        ▀▄      █   ████   ▐██ ▐██   ▐██   ▐██ ",
     "   ▌   ▐    ▀█████▄▀██     ▌     ▐   ▀▀▄▄▄▀▀   ▀▄▄▄▀▀    ▐▀▀    ▐▀▀ ▐▀▀   ▐▀▀   ▐▀▀ ",
     "   ▀   ▐      ▀█████ █ ",
-    "     ▀▄▐       ▀████ ", "       ▀         ▀ ", "",
-    "                                                             version: " ..
-        vim.fn.matchstr(vim.fn.execute('version'), [[NVIM v\zs[^\n]*]]) .. ""
+    "     ▀▄▐       ▀████ ",
+    "       ▀         ▀ ",
+    "",
+    "                                                             version: " .. vim.fn.matchstr(
+        vim.fn.execute("version"),
+        [[NVIM v\zs[^\n]*]]
+    ) .. "",
 }
 
-vim.g.dashboard_custom_footer = {""}
+vim.g.dashboard_custom_footer = { "" }
