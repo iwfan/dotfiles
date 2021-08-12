5568bd01c6f5fb2229d462dbdc005edcd96c5e2b
iwfan <i.wangfancn@gmail.com>
2021-08-12 12:05:11 +0800

    update neovim tree keymaps

--- nvim/lua/plugin/appearance/nvim-tree.lua
+++ nvim/lua/plugin/appearance/nvim-tree.lua
@@ -1,45 +1,44 @@
-local tree_cb = require "nvim-tree.config".nvim_tree_callback
+local tree_cb = require"nvim-tree.config".nvim_tree_callback
 var_tbl {
-  nvim_tree_width              = 40,
-  nvim_tree_indent_markers     = 1,
-  nvim_tree_follow             = 1,
-  nvim_tree_quit_on_open       = 1,
-  nvim_tree_git_hl             = 1,
-  nvim_tree_width_allow_resize = 1,
-  nvim_tree_ignore             = { ".git", ".cache", ".idea", ".DS_Store" },
-  nvim_tree_special_files      = { 'README.md', 'Makefile', 'MAKEFILE' },
-  nvim_tree_bindings           = {
-    { key = "l", cb = tree_cb("edit") },
-    { key = "h", cb = tree_cb("close_node") },
-  },
-  nvim_tree_icons              = {
-    default     = '',
-    symlink     = '',
-    git         = {
-      unstaged  = font_icon.modifiy,
-      staged    = font_icon.add,
-      unmerged  = font_icon.merge,
-      renamed   = font_icon.renamed,
-      untracked = font_icon.unknown,
-      deleted   = font_icon.deleted,
-      ignored   = font_icon.ignored
+    nvim_tree_width = 40,
+    nvim_tree_indent_markers = 1,
+    nvim_tree_follow = 0,
+    nvim_tree_quit_on_open = 1,
+    nvim_tree_git_hl = 1,
+    nvim_tree_width_allow_resize = 1,
+    nvim_tree_ignore = {".git", ".cache", ".idea", ".DS_Store"},
+    nvim_tree_special_files = {'README.md', 'Makefile', 'MAKEFILE'},
+    nvim_tree_bindings = {
+        {key = "l", cb = tree_cb("edit")},
+        {key = "h", cb = tree_cb("close_node")}
     },
-    folder      = {
-      default      = "",
-      open         = "",
-      empty        = "",
-      empty_open   = "",
-      symlink      = "",
-      symlink_open = "",
+    nvim_tree_icons = {
+        default = '',
+        symlink = '',
+        git = {
+            unstaged = font_icon.modifiy,
+            staged = font_icon.add,
+            unmerged = font_icon.merge,
+            renamed = font_icon.renamed,
+            untracked = font_icon.unknown,
+            deleted = font_icon.deleted,
+            ignored = font_icon.ignored
+        },
+        folder = {
+            default = "",
+            open = "",
+            empty = "",
+            empty_open = "",
+            symlink = "",
+            symlink_open = ""
+        }
     }
-  }
 }
 
-autocmd("BufEnter", "NvimTree",
-  table.concat {
-    [[if &ft == 'NvimTree' |]],
-    [[exec 'NvimTreeRefresh' | endif]]
-  }
-)
+autocmd("BufEnter", "NvimTree", table.concat {
+    [[if &ft == 'NvimTree' |]], [[exec 'NvimTreeRefresh' | endif]]
+})
 
 map_cmd("n|<leader>1", "NvimTreeToggle")
+map_cmd("n|<leader>e", "NvimTreeToggle")
+map_cmd("n|<leader>E", "NvimTreeFindFile")
