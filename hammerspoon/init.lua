dd554dfff07ea1fb3f0e6b5444365c16e4d9839e
iwfan <i.wangfancn@gmail.com>
2021-08-26 15:22:18 +0800

    enable pomodoro module

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,13 +1,13 @@
 _G.config_path = os.getenv("HOME") .. "/.hammerspoon/"
-_G.hyper = {"cmd", "ctrl", "shift"}
-_G.leader = {"alt"}
+_G.hyper = { "cmd", "ctrl", "shift" }
+_G.leader = { "alt" }
 
 -- 定义默认加载的 Spoons
 local hspoon_list = {
     "ModalMgr",
     "WinWin",
     "KSheet",
-    "VolumeScroll"
+    "VolumeScroll",
 }
 
 -- 加载 Spoons
@@ -15,15 +15,15 @@ for _, v in pairs(hspoon_list) do
     hs.loadSpoon(v)
 end
 
-require('modules.config-reload')
-require('modules.window-management')
-require('modules.clipboard')
-require('modules.app-launcher')
-require('modules.app-hints')
-require('modules.cheatsheet')
--- require('modules.pomodoro')
-require('modules.markdown')
-require('modules.search-text')
+require("modules.config-reload")
+require("modules.window-management")
+require("modules.clipboard")
+require("modules.app-launcher")
+require("modules.app-hints")
+require("modules.cheatsheet")
+require("modules.pomodoro")
+require("modules.markdown")
+require("modules.search-text")
 
 spoon.VolumeScroll:init()
 spoon.VolumeScroll:start(leader)
@@ -31,5 +31,7 @@ spoon.VolumeScroll:start(leader)
 spoon.ModalMgr.supervisor:bind(leader, "H", "查看 Hammerspoon 手册", function()
     hs.urlevent.openURL("https://www.hammerspoon.org/docs/")
 end)
-spoon.ModalMgr.supervisor:bind(leader, "Z", "Toggle Hammerspoon Console", function() hs.toggleConsole() end)
+spoon.ModalMgr.supervisor:bind(leader, "Z", "Toggle Hammerspoon Console", function()
+    hs.toggleConsole()
+end)
 spoon.ModalMgr.supervisor:enter()
