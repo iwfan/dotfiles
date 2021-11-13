9f4b32c87a7b64b6f73efda1ddb04e949fd2a163
iwfan <i.wangfancn@gmail.com>
2021-11-13 16:06:28 +0800

    update keymap

--- nvim/lua/p/nvim-bufferline/init.lua
+++ nvim/lua/p/nvim-bufferline/init.lua
@@ -8,10 +8,13 @@ require("bufferline").setup {
     },
 }
 
-map_cmd("n|[b", "BufferLineCyclePrev")
-map_cmd("n|]b", "BufferLineCycleNext")
 map_cmd("n|<M-{>", "BufferLineCyclePrev")
 map_cmd("n|<M-}>", "BufferLineCycleNext")
+map_cmd("n|C<Tab>", "BufferLinePick")
+map_cmd("n|<M-w>", "Bdelete!")
+
+map_cmd("n|[b", "BufferLineCyclePrev")
+map_cmd("n|]b", "BufferLineCycleNext")
 map_cmd("n|[B", "BufferLineMovePrev")
 map_cmd("n|]B", "BufferLineMoveNext")
 map_cmd("n|<leader>b", "BufferLinePick")
