e5727ef58e3b82d3f427943563710fd4d5eeb188
iwfan <i.wangfancn@gmail.com>
2022-03-15 09:17:32 +0800

    update dotfiles

--- lf/pv.sh
+++ lf/pv.sh
@@ -5,7 +5,7 @@
 case "$(file --dereference --brief --mime-type -- "$1")" in
   # image/*) kitty +kitten icat "$1" ;;
   # image/*) viu "$1" ;;  # It's working but it's not the best way.
-  text/* | */xml) bat --terminal-width "$4" -f "$1" ;;
+  text/* | */xml | application/json) bat --terminal-width "$4" -f "$1" ;;
 esac
 
 exit 1
