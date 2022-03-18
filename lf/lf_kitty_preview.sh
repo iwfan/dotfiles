b682915ed118fd4d89086ba3ab739e30218bc446
iwfan <i.wangfancn@gmail.com>
2022-03-18 10:12:55 +0800

    update lf config

---
+++ lf/lf_kitty_preview.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+file=$1
+w=$2
+h=$3
+x=$4
+y=$5
+
+# echo $( file -Lb --mime-type $file)
+
+if [[ "$( file -Lb --mime-type "$file")" =~ ^image ]]; then
+    kitty +icat --silent --transfer-mode file --place "${w}x${h}@${x}x${y}" "$file"
+    exit 1
+fi
+
+if [[ "$( file -Lb --mime-type "$file")" =~ ^text ]]; then
+    bat --terminal-width "$4" -f "$1"
+    exit 1
+fi
+
+if [[ "$( file -Lb --mime-type "$file")" =~ xml$ ]]; then
+    bat --terminal-width "$4" -f "$1"
+    exit 1
+fi
+
+if [[ "$( file -Lb --mime-type "$file")" =~ json$ ]]; then
+    bat --terminal-width "$4" -f "$1"
+    exit 1
+fi
