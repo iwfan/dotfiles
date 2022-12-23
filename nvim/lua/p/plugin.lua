0d359696f929eefdf3d015b8b9035b11db4a5a56
iwfan <i.wangfancn@gmail.com>
2022-12-23 19:43:17 +0800

    update neovim config

--- nvim/lua/p/plugin.lua
+++ nvim/lua/p/plugin.lua
@@ -1,23 +1,18 @@
-local fn = vim.fn
-local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
-local packer_bootstrap = nil
-if fn.empty(fn.glob(install_path)) > 0 then
-    packer_bootstrap =
-        fn.system { "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path }
-    vim.cmd [[packadd packer.nvim]]
+-- Install packer
+local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
+local is_bootstrap = false
+if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+  is_bootstrap = true
+  vim.fn.system { 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path }
+  vim.cmd [[packadd packer.nvim]]
 end
 
 require("packer").startup {
     function(use)
         use { "wbthomason/packer.nvim" }
-        use {
-            "antoinemadec/FixCursorHold.nvim",
-            setup = function()
-                vim.g.cursorhold_updatetime = 300
-            end,
-        }
 
-        use { -- Appearance
+        -- Appearance
+        use {
             {
                 "glepnir/dashboard-nvim",
                 config = "require('p.nvim-startup')",
@@ -187,3 +182,25 @@ require("packer").startup {
         compile_on_sync = true,
     },
 }
+
+
+-- When we are bootstrapping a configuration, it doesn't
+-- make sense to execute the rest of the init.lua.
+--
+-- You'll need to restart nvim, and then it will work.
+if is_bootstrap then
+  print '=================================='
+  print '    Plugins are being installed'
+  print '    Wait until Packer completes,'
+  print '       then restart nvim'
+  print '=================================='
+  return
+end
+
+-- Automatically source and re-compile packer whenever you save this init.lua
+local packer_group = vim.api.nvim_create_augroup('Packer', { clear = true })
+vim.api.nvim_create_autocmd('BufWritePost', {
+  command = 'source <afile> | PackerCompile',
+  group = packer_group,
+  pattern = vim.fn.expand '$MYVIMRC',
+})
