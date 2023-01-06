2412aaf51c4a62d30956ee0001c0e673163c2b95
iwfan <i.wangfancn@gmail.com>
2023-01-06 14:20:44 +0800

    update lf config

---
+++ fish/functions/lfi.fish
@@ -0,0 +1,14 @@
+function lfi
+    lf
+    set file "/Users/f/dotfiles/lf/temp_dir"
+    if test -f "$file"
+        set dir (cat $file)
+        /bin/rm -f $file
+        if test -d "$dir"
+            if test "$dir" != (pwd)
+                commandline -a "cd $dir"
+                commandline -f execute
+            end
+        end
+    end
+end
