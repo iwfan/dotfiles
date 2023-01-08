521aa13be2249b800ff59393b5c905770df00fd0
iwfan <i.wangfancn@gmail.com>
2023-01-08 21:09:10 +0800

    update preview

--- lf/lf_kitty_preview.sh
+++ lf/lf_kitty_preview.sh
@@ -1,29 +1,28 @@
 #!/usr/bin/env bash
-
 file=$1
 w=$2
 h=$3
 x=$4
 y=$5
 
-# echo $( file -Lb --mime-type $file)
+filetype="$( file -Lb --mime-type "$file")"
 
-if [[ "$( file -Lb --mime-type "$file")" =~ ^text ]]; then
+if [[ "$filetype" =~ ^text ]]; then
     bat --terminal-width "$4" -f "$1"
     exit 1
 fi
 
-if [[ "$( file -Lb --mime-type "$file")" =~ xml$ ]]; then
+if [[ "$filetype" =~ xml$ ]]; then
     bat --terminal-width "$4" -f "$1"
     exit 1
 fi
 
-if [[ "$( file -Lb --mime-type "$file")" =~ json$ ]]; then
+if [[ "$filetype" =~ json$ ]]; then
     bat --terminal-width "$4" -f "$1"
     exit 1
 fi
 
-if [[ "$( file -Lb --mime-type "$file")" =~ ^image ]]; then
+if [[ "$filetype" =~ ^image ]]; then
     kitty +icat --silent --transfer-mode file --place "${w}x${h}@${x}x${y}" "$file"
     exit 1
 fi
