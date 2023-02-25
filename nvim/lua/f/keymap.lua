f7fc6ed9288c29f98d42db324703194073f357e5
iwfan <i.wangfancn@gmail.com>
2023-02-25 22:48:09 +0800

    set ctrl k in insert mode

--- nvim/lua/f/keymap.lua
+++ nvim/lua/f/keymap.lua
@@ -4,7 +4,6 @@ vim.g.maplocalleader = " "
 -- better up/down
 vim.keymap.set("n", "k", [[(v:count == 0 ? 'gk' : 'k')]], { expr = true })
 vim.keymap.set("n", "j", [[(v:count == 0 ? 'gj' : 'j')]], { expr = true })
-
 -- https://github.com/mhinz/vim-galore#saner-behavior-of-n-and-n
 vim.keymap.set("n", "n", "'Nn'[v:searchforward]", { expr = true, desc = "Next search result" })
 vim.keymap.set("x", "n", "'Nn'[v:searchforward]", { expr = true, desc = "Next search result" })
@@ -12,25 +11,24 @@ vim.keymap.set("o", "n", "'Nn'[v:searchforward]", { expr = true, desc = "Next se
 vim.keymap.set("n", "N", "'nN'[v:searchforward]", { expr = true, desc = "Prev search result" })
 vim.keymap.set("x", "N", "'nN'[v:searchforward]", { expr = true, desc = "Prev search result" })
 vim.keymap.set("o", "N", "'nN'[v:searchforward]", { expr = true, desc = "Prev search result" })
-
 -- Add undo break-points
 vim.keymap.set("i", ",", ",<c-g>u")
 vim.keymap.set("i", ".", ".<c-g>u")
 vim.keymap.set("i", ";", ";<c-g>u")
-
+-- for short finger
 vim.keymap.set({ "n", "v", "o" }, "H", "^")
 vim.keymap.set({ "n", "v", "o" }, "L", "g_")
 vim.keymap.set({ "n", "v", "o" }, "M", "%")
 vim.keymap.set({ "n", "v", "o" }, "]w", "g*")
 vim.keymap.set({ "n", "v", "o" }, "[w", "g#")
 
-vim.keymap.set("n", "q", "")
+vim.keymap.set("n", "q", "<Nop>")
 vim.keymap.set("n", "gq", "q")
-vim.keymap.set("n", "<leader>qq", "<cmd>qa<cr>", { desc = "Quit all" })
+vim.keymap.set("n", "<space>qq", "<cmd>qa<cr>", { desc = "Quit all" })
 vim.keymap.set("n", "<space><bs>", '"_dd')
-vim.keymap.set("n", "[<space>", ":<c-u>put! =repeat(nr2char(10), v:count1)<cr>'[")
-vim.keymap.set("n", "]<space>", ":<c-u>put =repeat(nr2char(10), v:count1)<cr>")
-vim.keymap.set({ "i", "n" }, "<esc>", "<cmd>noh<cr><esc>", { desc = "Escape and clear hlsearch" })
+vim.keymap.set("n", "[<space>", ":<c-u>put! =repeat(nr2char(10), v:count1)<cr>'[", { silent = true })
+vim.keymap.set("n", "]<space>", ":<c-u>put =repeat(nr2char(10), v:count1)<cr>", { silent = true })
+vim.keymap.set("n" , "<esc>", "<cmd>noh<cr><esc>", { desc = "Escape and clear hlsearch" })
 
 vim.keymap.set("i", "<C-a>", "<C-o>^")
 vim.keymap.set("c", "<C-a>", "<HOME>")
@@ -38,6 +36,7 @@ vim.keymap.set("!", "<C-b>", "<Left>")
 vim.keymap.set("!", "<C-e>", "<END>")
 vim.keymap.set("!", "<C-f>", "<Right>")
 vim.keymap.set("!", "<C-v>", "<C-r>*")
+vim.keymap.set("!", "<C-k>", "<C-o>\"_d$")
 vim.keymap.set("!", "<M-f>", "<S-Right>")
 vim.keymap.set("!", "<M-b>", "<S-Left>")
 vim.keymap.set("i", "<S-Tab>", "<BS>")
@@ -69,8 +68,8 @@ vim.keymap.set("n", "<A-k>", ":m .-2<cr>==", { desc = "Move up" })
 vim.keymap.set("v", "<A-k>", ":m '<-2<cr>gv=gv", { desc = "Move up" })
 vim.keymap.set("i", "<A-k>", "<Esc>:m .-2<cr>==gi", { desc = "Move up" })
 
-vim.keymap.set("n", "\\q", ":<c-u>copen<cr>")
-vim.keymap.set("n", "<bs>q", ":<c-u>cclose<cr>")
+vim.keymap.set("n", "<Bslash>q", ":<c-u>copen<cr>")
+vim.keymap.set("n", "<BS>q", ":<c-u>cclose<cr>")
 vim.keymap.set("n", "[q", ":<c-u>cprevious<cr>")
 vim.keymap.set("n", "]q", ":<c-u>cnext<cr>")
 
