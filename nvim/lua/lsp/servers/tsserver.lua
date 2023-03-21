362a952f8d40bddb98234a996e9c6c63903528a6
iwfan <i.wangfancn@gmail.com>
2023-03-21 13:06:29 +0800

    update lsp config

--- nvim/lua/lsp/servers/tsserver.lua
+++ nvim/lua/lsp/servers/tsserver.lua
@@ -2,10 +2,11 @@ return function(on_attach, capabilities)
     require("typescript").setup {
         disable_commands = false, -- prevent the plugin from creating Vim commands
         debug = false, -- enable debug logging for commands
-        server = { -- pass options to lspconfig's setup method
+        server = {
+            -- pass options to lspconfig's setup method
             on_attach = function(client, bufnr)
-                client.server_capabilities.document_formatting = false
-                client.server_capabilities.document_range_formatting = false
+                client.server_capabilities.document_formatting = true
+                client.server_capabilities.document_range_formatting = true
                 on_attach(client, bufnr)
             end,
             capabilities = capabilities,
