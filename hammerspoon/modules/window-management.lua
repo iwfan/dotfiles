2b276436efdf2eb98b932a96aa86fba7112dd5c2
iwfan <i.wangfancn@gmail.com>
2023-08-07 18:07:43 +0800

    add function to move window to another desktop

--- hammerspoon/modules/window-management.lua
+++ hammerspoon/modules/window-management.lua
@@ -49,6 +49,33 @@ local KEY_BINDINGS = {
         f.h = max.h
         win:setFrame(f)
     end, false, },
+    { HYPER, "]", "Next Desktop", function()
+        local win = hs.window.focusedWindow()
+        local all_spaces = hs.spaces.spacesForScreen(hs.screen.mainScreen():id())
+        -- local active_space = hs.spaces.activeSpaceOnScreen(hs.screen.mainScreen():id())
+        local focus_space = hs.spaces.focusedSpace()
+        local index = hs.fnutils.indexOf(all_spaces, focus_space)
+        if index == #all_spaces then
+            return
+        end
+        local next_space_id = all_spaces[index + 1]
+        hs.spaces.moveWindowToSpace(win:id(), next_space_id)
+        hs.spaces.gotoSpace(next_space_id)
+    end, false, },
+    { HYPER, "[", "Prev Desktop", function()
+        local win = hs.window.focusedWindow()
+        local all_spaces = hs.spaces.spacesForScreen(hs.screen.mainScreen():id())
+        -- local active_space = hs.spaces.activeSpaceOnScreen(hs.screen.mainScreen():id())
+        local focus_space = hs.spaces.focusedSpace()
+        local index = hs.fnutils.indexOf(all_spaces, focus_space)
+        if index == 1 then
+            return
+        end
+        local prev_space_id = all_spaces[index - 1]
+        hs.spaces.moveWindowToSpace(win:id(), prev_space_id)
+        hs.spaces.gotoSpace(prev_space_id)
+    end, false, },
+
 }
 -- stylua: ignore end
 
