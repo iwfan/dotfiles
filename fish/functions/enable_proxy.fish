8b1e0230062ed0ac0cbc4c5c8f80ab3b753d8c7f
iwfan <iwfan@duck.com>
2024-08-09 18:30:39 +0800

    update dotfiles

--- fish/functions/enable_proxy.fish
+++ fish/functions/enable_proxy.fish
@@ -1,7 +1,7 @@
 function enable_proxy -d "turn on proxy"
-  set -gx https_proxy "http://127.0.0.1:7890"
-  set -gx http_proxy "http://127.0.0.1:7890"
-  set -gx all_proxy "socks5://127.0.0.1:7890"
+  set -gx https_proxy "http://127.0.0.1:7897"
+  set -gx http_proxy "http://127.0.0.1:7897"
+  set -gx all_proxy "socks5://127.0.0.1:7897"
 
   echo "proxy is on, $http_proxy"
 end
