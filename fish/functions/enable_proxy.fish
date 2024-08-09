960995f275df501954df112793e3a6504278e739
iwfan <iwfan@duck.com>
2024-08-09 18:44:32 +0800

    add npm proxy

--- fish/functions/enable_proxy.fish
+++ fish/functions/enable_proxy.fish
@@ -3,5 +3,8 @@ function enable_proxy -d "turn on proxy"
   set -gx http_proxy "http://127.0.0.1:7897"
   set -gx all_proxy "socks5://127.0.0.1:7897"
 
+  npm config set proxy "http://127.0.0.1:7897"
+  npm config set https-proxy "http://127.0.0.1:7897"
+
   echo "proxy is on, $http_proxy"
 end
