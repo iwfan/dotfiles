b00fba9b8d1b0b8c71d14190f0962d17c4076ad8
iwfan <iwfan@duck.com>
2024-08-23 16:53:49 +0800

    add gnome shell settings

---
+++ gnome/setup.sh
@@ -0,0 +1,28 @@
+# Install Gnome shell manager
+# sudo apt install -y gnome-shell-extension-manager
+
+# Caffeine
+# Clipboard Incicator
+# Color Picker
+# Space bar
+# Vitals
+# Just Perfection
+# Forge
+# Emoji Copy
+# Coverflow Alt-Tab
+
+
+# Center new windows in the middle of the screen
+gsettings set org.gnome.mutter center-new-windows true
+
+# Use 6 fixed workspaces instead of dynamic mode
+gsettings set org.gnome.mutter dynamic-workspaces false
+gsettings set org.gnome.desktop.wm.preferences num-workspaces 6
+gsettings set org.gnome.desktop.wm.keybindings close "['<Super>w']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-1 "['<Super>1']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-2 "['<Super>2']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-3 "['<Super>3']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-4 "['<Super>4']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-5 "['<Super>5']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-6 "['<Super>6']"
+
