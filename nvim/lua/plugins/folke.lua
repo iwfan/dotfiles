cc5dc3aa610248fbfef8e422d17938f95bc419c1
iwfan <iwfan@duck.com>
2024-09-13 20:57:56 +0800

    update dot files

--- nvim/lua/plugins/folke.lua
+++ nvim/lua/plugins/folke.lua
@@ -3,7 +3,7 @@ return {
         "folke/which-key.nvim",
         event = "VeryLazy",
         opts = {
-            window = {
+            win = {
                 border = { "┏", "━", "┓", "┃", "┛", "━", "┗", "┃" },
             },
         },
