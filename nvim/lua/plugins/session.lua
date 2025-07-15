4562d0cc45b5d0a8ab409dd8c44b77bf8e4c7b10
iwfan <iwfan@duck.com>
2025-07-15 18:12:28 +0800

    feat: update lsp config

--- nvim/lua/plugins/session.lua
+++ nvim/lua/plugins/session.lua
@@ -1,13 +1,3 @@
-local function get_session_name()
-    local name = vim.fn.getcwd()
-    local branch = vim.fn.system "git branch --show-current"
-    if vim.v.shell_error == 0 then
-        return name .. branch
-    else
-        return name
-    end
-end
-
 vim.api.nvim_create_user_command("ReadSession", function()
     require("persistence").load()
 end, { force = true, nargs = 0 })
