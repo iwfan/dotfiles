141b13930411c52ca99ac1c6e386d1c31ed005dd
iwfan <iwfan@duck.com>
2025-07-22 11:53:20 +0800

    feat: setup a proxy first

--- fish/functions/google.fish
+++ fish/functions/google.fish
@@ -3,5 +3,5 @@ function google -d "Search with gemini" -a query
     echo "Need a string to search for!"
     return 1
   end
-  gemini -p "Make a google search about '$query' and summrize result as markdown format. Be concise!" | glow
+  https_proxy=http://127.0.0.1:7890 http_proxy=http://127.0.0.1:7890 gemini -p "Make a google search about '$query' and summrize result as markdown format. Be concise!" | glow
 end
