ee4cddde0514f4933a6ae2a995c0c0d56edbf5e4
iwfan <iwfan@duck.com>
2025-07-30 17:32:24 +0800

    feat: update kitty config

--- nvim/lua/config/usercmds.lua
+++ nvim/lua/config/usercmds.lua
@@ -14,5 +14,5 @@ vim.api.nvim_create_user_command("H", function(args)
 end, { nargs = 1, complete = "help" })
 
 vim.api.nvim_create_user_command("Format", function()
-    vim.lsp.buf.format()
+    require("conform").format { lsp_fallback = true }
 end, { nargs = 0 })
