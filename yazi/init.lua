d1f84c0e013fee4b50cdb3acf33a8c7fc158d1f0
iwfan <iwfan@duck.com>
2026-01-02 10:10:07 +0800

    feat: show full border for yazi

--- yazi/init.lua
+++ yazi/init.lua
@@ -1,7 +1,7 @@
--- require("full-border"):setup {
---   -- Available values: ui.Border.PLAIN, ui.Border.ROUNDED
---   type = ui.Border.ROUNDED,
--- }
+require("full-border"):setup {
+  -- Available values: ui.Border.PLAIN, ui.Border.ROUNDED
+  type = ui.Border.ROUNDED,
+}
 require("no-status"):setup()
 
 function Linemode:size_and_mtime()
