16a28fc53d0aa253e18f49397b603e3de0246f44
iwfan <iwfan@duck.com>
2026-04-01 18:14:40 +0800

    feat: add agent shell script

---
+++ agent.sh
@@ -0,0 +1,6 @@
+curl -fsSL https://claude.ai/install.sh | bash
+npm install -g @google/gemini-cli
+npm install -g @mariozechner/pi-coding-agent
+npm install -g @openai/codex
+npm install -g wrangler
+
