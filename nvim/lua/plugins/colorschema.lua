a8d19134292a67a072cf66f8f424516da70b5827
iwfan <iwfan@duck.com>
2026-07-22 12:20:24 +0800

    feat: update colorschema

--- nvim/lua/plugins/colorschema.lua
+++ nvim/lua/plugins/colorschema.lua
@@ -1,13 +1,22 @@
 return {
     {
         "sainnhe/everforest",
-        version = false,
         lazy = false,
         priority = 1000,
         config = function()
             vim.g.everforest_background = "hard"
             vim.g.everforest_better_performance = 1
             vim.cmd.colorscheme "everforest"
+        end,
+    },
+    {
+        "sainnhe/gruvbox-material",
+        lazy = false,
+        priority = 1000,
+        config = function()
+            vim.g.gruvbox_material_background = "hard"
+            vim.g.gruvbox_material_better_performance = 1
+            -- vim.cmd.colorscheme "gruvbox-material"
         end,
     },
 }
