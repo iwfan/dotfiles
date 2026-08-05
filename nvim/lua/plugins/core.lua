11a5d85a800d1d149b95a9d25cc9771a8895ee8e
iwfan <iwfan@duck.com>
2026-08-06 07:07:39 +0800

    feat: update yazi config

--- nvim/lua/plugins/core.lua
+++ nvim/lua/plugins/core.lua
@@ -315,21 +315,16 @@ return {
         keys = {
             -- 👇 in this section, choose your own keymappings!
             {
-                "<leader>-",
+                "<C-t>",
                 mode = { "n", "v" },
-                "<cmd>Yazi cwd<cr>",
+                "<cmd>Yazi<cr>",
                 desc = "Open yazi at the current file",
             },
             {
                 -- Open in the current working directory
-                "<leader>cw",
+                "<leader>-",
                 "<cmd>Yazi cwd<cr>",
                 desc = "Open the file manager in nvim's working directory",
-            },
-            {
-                "<c-t>",
-                "<cmd>Yazi toggle<cr>",
-                desc = "Resume the last yazi session",
             },
         },
         ---@type YaziConfig | {}
